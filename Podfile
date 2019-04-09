529d6818a72d7b94250e5129aa3b98b43ba4f9f7
wannagohome <iwwuf7@gmail.com>
2019-04-09 18:19:16 +0900

    Starting Point

---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://github.com/CocoaPods/Specs.git'
+target 'Task' do
+pod 'GithubAPI'
+pod 'SwiftyJSON', '~> 4.0'
+pod 'Alamofire', '~> 5.0.0-beta.2'
+pod 'Kingfisher', '~> 5.0'
+end
---
+++ Task/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Peter Jang on 09/04/2019.
+//  Copyright © 2019 Peter Jang. All rights reserved.
+//
+
+import UIKit
+import GithubAPI
+import SwiftyJSON
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
