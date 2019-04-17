7a5203ee39ffbec9cb46c4c60c254ae163b4d5d7
wannagohome <iwwuf7@gmail.com>
2019-04-17 10:20:36 +0900

    remove useless API

--- Task/ViewController.swift
+++ Task/ViewController.swift
@@ -38,12 +38,6 @@ class ViewController: UIViewController {
         setupTableView()
         setupNextPageLoading()
         driveToTableViewItems()
-        
-        ViewModel.shared.tableContentOffset
-            .subscribe{
-                print($0)
-        }
-        .disposed(by: disposeBag)
     }
     
     
